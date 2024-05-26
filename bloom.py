2bdc93a434351c184eaf89ed2648894d6157aeed
pianist-coder <101991255+pianist-coder@users.noreply.github.com>
2024-05-26 18:27:04 +0300

    Add files via upload

---
+++ bloom.py
@@ -0,0 +1,126 @@
+#-------------------------------------------------------------------------
+# Name:        BLOOM FOR KSOLVER X
+# Author:      pianist (Telegram: @pianist_coder)
+# Credit:      iceland
+# Created:     24.05.2024
+# Copyright:   (c) pianist 2022-2024
+#-------------------------------------------------------------------------
+
+import os, sys, xxhash, random, time
+import secp256k1 as btc
+from pybloomfilter import BloomFilter
+from sys import argv
+from multiprocessing import Process, cpu_count, Value, Event
+
+#=========================================================================
+class color:
+    GREEN = '\033[32m'
+    RED = '\033[31m'
+    X = '\033[5m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
+#=========================================================================
+bsplash = '''
+█▀▄ █░░ ▄▀▄ ▄▀▄ █▄░▄█     █▀ ▀ █░░ ▀█▀ █▀▀ █▀▀▄ 
+█▀█ █░▄ █░█ █░█ █░█░█     █▀ █ █░▄ ░█░ █▀▀ █▐█▀ 
+▀▀░ ▀▀▀ ░▀░ ░▀░ ▀░░░▀     ▀░ ▀ ▀▀▀ ░▀░ ▀▀▀ ▀░▀▀ 
+'''
+#=========================================================================
+(count, bloom_filter_name, filebase, bit, core) = (int(argv[1]), argv[2], argv[3], int(argv[4]), int(argv[5]))
+start = bit - 1
+end = bit
+if os.path.exists(bloom_filter_name):
+    bf = BloomFilter.open(bloom_filter_name)
+else:
+    bf = BloomFilter(count, 0.0000000001, bloom_filter_name)
+#b = open(filebase, 'a')
+st = time.time()
+#=========================================================================
+
+def pr():
+    os.system('cls||clear')
+    print(color.GREEN + bsplash + color.END)
+    print(color.RED + 'by pianist (Telegram: @pianist_coder | btc: bc1q0jth0rjaj2vqtqgw45n39fg4qrjc37hcw4frcz)' + color.END)
+    print(color.BOLD + '\n[+] Program started' + color.END)
+    print("-"*87)
+    print(f'[+] Items: {count}')
+    print(f'[+] Cores: {core}')
+    print("-"*87)
+    
+def generate_random_bloom(start, end):
+    rnd = {}
+    for _ in range(10000):
+        x = random.randint(2 ** start - (2 ** start - 5), 2 ** end - 1)
+        P = btc.scalar_multiplication(x)
+        rnd[P] = f'{x:x}'
+    return rnd
+
+def scan_str(num):
+    suffixes = ["", "K", "M", "B", "T"]
+    exponent = 0
+    while abs(num) >= 1000 and exponent < 4:
+        num /= 1000
+        exponent += 1
+    return f"{num:.2f} {suffixes[exponent]}"
+
+def display_time(seconds):
+    hours, rem = divmod(seconds, 3600)
+    minutes, seconds = divmod(rem, 60)
+    return f"{int(hours):02d}:{int(minutes):02d}:{seconds:05.2f}"
+
+def speedup(st, counter):
+    speed = counter / (time.time() - st)
+    print(f'[{scan_str(counter)}] [{scan_str(speed)}keys] [{display_time(time.time() - st)}]      ', end = '\r')
+    
+def bloom_start(cores='all'):
+    try:
+        available_cores = cpu_count()
+        if cores == 'all': cores = available_cores
+        elif 0 < int(cores) <= available_cores: cores = int(cores)
+        else: cores = 1
+        counter = Value('L')
+        workers = []
+        match = Event()
+        for r in range(cores):
+            p = Process(target=bloom_create, args=(counter, r, match))
+            workers.append(p)
+            p.start()
+        for worker in workers:
+            worker.join()
+    except(KeyboardInterrupt, SystemExit):
+        exit('\nSIGINT or CTRL-C detected. Exiting gracefully. BYE')
+    sys.stdout.write('\n\n[+] Bloom creating complete in {0:.2f} sec\n'.format(time.time() - st))
+
+def save_data(data, filename):
+    with open(filename, "a") as f:
+        for item, value in data.items():
+            f.write(f'{value};{xxhash.xxh64(item).hexdigest()}\n')
+            bf.add(item)
+            
+def bloom_create(counter, r, match):
+    st = time.time()
+    while not match.is_set():
+        if match.is_set(): return
+        temp = generate_random_bloom(start, end)
+        save_data(temp, filebase)
+        with counter.get_lock(): counter.value += 10000
+        if counter.value % 1000000 == 0:
+            speedup(st, counter.value)
+        if counter.value == count - (core-1)*10000:
+            match.set()
+
+            
+#=========================================================================
+pr()
+bloom_start(cores=core)
+
+'''
+
+use:
+
+pip install pybloomfiltermmap3
+pip install xxhash
+python3 bloom.py 10000000 60 60.txt 60 10
+
+'''
---
+++ ksolverx.py
@@ -0,0 +1,166 @@
+#-------------------------------------------------------------------------
+# Name:        KSOLVER X
+# Author:      pianist (Telegram: @pianist_coder)
+# Credit:      iceland
+# Created:     24.05.2024
+# Copyright:   (c) pianist 2022-2024
+#-------------------------------------------------------------------------
+
+import sys, time, xxhash, pybloomfilter, os
+import secp256k1 as btc
+import random
+from multiprocessing import Event, Process, Queue, Value, cpu_count
+from math import log2
+
+#=========================================================================
+splash = """
+█░▄▀ ▄▀▀ ▄▀▄ █░░ ▐▌░▐▌ █▀▀ █▀▀▄     █░█ 
+█▀▄░ ░▀▄ █░█ █░▄ ░▀▄▀░ █▀▀ █▐█▀     ▄▀▄ 
+▀░▀▀ ▀▀░ ░▀░ ▀▀▀ ░░▀░░ ▀▀▀ ▀░▀▀     ▀░▀ 
+"""
+sp = """
+-----------------------------------------------------
+{0:x}
+-----------------------------------------------------
+{1}
+{2}
+-----------------------------------------------------"""
+#=========================================================================
+class color:
+    GREEN = "\033[32m"
+    RED = "\033[31m"
+    X = "\033[5m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+    END = "\033[0m"
+#=========================================================================
+k, blname, basefile, rng, c = sys.argv[1:]
+
+bloom = pybloomfilter.BloomFilter.open(blname)
+a = btc.pub2upub(k)
+l = bloom.capacity
+st = time.time()
+rng = int(rng)
+c = int(c)
+start = 2 ** (rng - 1)
+end = 2 ** rng - 1
+#=========================================================================
+
+def p_2(num):
+    return f'{log2(num):.2f}'
+
+def pr(fc):
+    os.system("cls||clear")
+    printc(color.GREEN, splash)
+    printc(color.RED, "by pianist (Telegram: @pianist_coder | btc: bc1q0jth0rjaj2vqtqgw45n39fg4qrjc37hcw4frcz)")
+    printc(color.BOLD, "\n[+] Program started")
+    print("-"*87)
+    print(f"[+] Pubkey:          {btc.point_to_cpub(a).upper()}")
+    print(f"[+] Bloom items:     {l}")
+    print(f"[+] Key range:       {rng - 1} bit")
+    print(f"[+] Cores:           {c}")
+    print(f"[+] Bloom collision: {fc}")
+    print("-"*87)
+
+def printc(colors, message):
+    print(colors + message + color.END)
+    
+def speedup_prob(st, counter, l, rng):
+    elapsed_time = time.time() - st
+    speed = counter / elapsed_time
+    prob = 100 * (1 - (1 - l / 2 ** (rng - 1)) ** counter)
+    print(f"[2^{p_2(counter)}] [{scan_str(speed)}keys] [{display_time(elapsed_time)}] [Prob: {prob:.20f}%]     ", end="\r")
+
+def scan_str(num):
+    suffixes = ["", "K", "M", "B", "T"]
+    exponent = 0
+    while abs(num) >= 1000 and exponent < 4:
+        num /= 1000
+        exponent += 1
+    return f"{num:.2f} {suffixes[exponent]}"
+
+def display_time(seconds):
+    hours, rem = divmod(seconds, 3600)
+    minutes, seconds = divmod(rem, 60)
+    return f"{int(hours):02d}:{int(minutes):02d}:{seconds:05.2f}"
+
+def find(word, file):
+    with open(file, "r") as f:
+        for line in f:
+            if word in line:
+                return int(line.split(";")[0], 16)
+    return None
+
+def chunks(s):
+    for start in range(0, 66560, 65):
+        yield s[start : start + 65]
+
+def key_solver(cores="all"):
+    available_cores = cpu_count()
+    if cores == "all": cores = available_cores
+    elif 0 < int(cores) <= available_cores: cores = int(cores)
+    else: cores = 1
+    counter = Value("Q")
+    fc = Value("L")
+    match = Event()
+    queue = Queue()
+    workers = []
+    for r in range(cores):
+        p = Process(target=solve_keys, args=(counter, fc, match, queue, r))
+        workers.append(p)
+        p.start()
+    for worker in workers:
+        worker.join()
+    private_key = queue.get()
+    print(sp.format(private_key, btc.privatekey_to_address(0, True, private_key), btc.btc_pvk_to_wif(private_key)))
+    printc(color.BOLD, f"[+] Complete in {time.time() - st:.2f} sec")
+
+def solve_keys(counter, fc, match, queue, r):
+    while not match.is_set():
+        step = random.randint(2**(rng-5), 2**(rng-4))
+     #   step = random.randint(2**(rng-6), 2**(rng-5)) ### do not go beyond the range
+        a_ = btc.point_subtraction(a, btc.scalar_multiplication(step))
+     #   with open('possible.txt', 'a') as found: ### you may save result xpoints for future
+     #       found.write(f'{a_.hex()[2:66]} # + {step:x}\n')
+        with counter.get_lock():
+            counter.value += 2048
+        for i1, item in enumerate(chunks(btc.point_sequential_increment(1024, a_))):
+            if item in bloom:
+                process_collision(item, i1 + 1, counter, fc, match, queue, r, basefile, "addition", step)
+                if match.is_set(): return
+        for i2, item in enumerate(chunks(btc.point_sequential_decrement(1024, a_))):
+            if item in bloom:
+                process_collision(item, i2 + 1, counter, fc, match, queue, r, basefile, "subtraction", step)
+                if match.is_set(): return
+        if counter.value % 1000000 == 0:
+            speedup_prob(st, counter.value, l, rng)
+
+def process_collision(item, i, counter, fc, match, queue, r, basefile, sign, step):
+    with counter.get_lock():
+        fc.value += 1
+    printc(color.BOLD, "\n[+] Bloom collision...")
+    p1 = find(xxhash.xxh64(item).hexdigest(), basefile)
+    if p1:
+        offset = p1 - i + step if sign == "addition" else p1 + i + step
+        with open('FOUND.txt', 'a') as found:
+            found.write(f'{a.hex()};{offset:x}\n')
+        printc(color.BOLD, f"\n[+] Core#{r} solved key by {sign} with step " + hex(step)[2:])
+        match.set()
+        queue.put_nowait(offset)
+    else:
+        printc(color.BOLD, "\n[+] The key is not found...\n")
+        pr(fc.value) 
+
+#=========================================================================
+pr(0)
+key_solver(cores=c)
+
+'''
+
+use:
+
+pip install pybloomfiltermmap3
+pip install xxhash
+python3 ksolverx.py 030c7557639c597ba58ab73d9e4f78621e14a247ed805c215b8dbfa085a0ad8487 60 60.txt 60 10
+
+'''
